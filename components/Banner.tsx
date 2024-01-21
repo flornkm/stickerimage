fa23b0c5ab1d64e2eab08bef86d6ab70140d4235
flooorianflo <florian.kiem@hfg.design>
2024-01-21 23:57:09 +0100

    fix: PH banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -9,15 +9,17 @@ export default function Banner() {
     <Link
       href="https://www.producthunt.com/posts/memoji-laptop-sticker-maker"
       target="_blank"
-      className="fixed top-0 w-screen z-50 bg-[#FF6154] text-white font-medium text-center py-3"
+      className="fixed top-0 w-screen z-50 bg-[#FF6154] text-white font-medium text-center py-2 px-4"
     >
-      {currentDate > showDate
-        ? "This project is currently live on Product Hunt! "
-        : "This project will be live on Product Hunt soon! "}
-      I would love your support.
-      <span className="bg-white ml-2 text-[#FF6154] px-2 py-1 rounded-full hover:text-white hover:bg-transparent transition-all">
-        Click here to check it out
-      </span>
+      <p className="truncate py-1">
+        {currentDate > showDate
+          ? "This project is currently live on Product Hunt! "
+          : "This project will be live on Product Hunt soon! "}
+        I would love your support.
+        <span className="bg-white ml-2 text-[#FF6154] px-2 py-1 rounded-full hover:text-white hover:bg-transparent transition-all">
+          Click here to check it out
+        </span>
+      </p>
     </Link>
   ) : (
     <></>
